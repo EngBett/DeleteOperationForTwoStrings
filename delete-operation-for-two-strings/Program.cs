cc1cf5999a12d034607ddc5e00ce19dc0e7d25b4
Ronald Bett <rbett@jambopay.com>
2021-10-16 07:02:49 +0300

    Initial commit

---
+++ delete-operation-for-two-strings/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace delete_operation_for_two_strings
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
